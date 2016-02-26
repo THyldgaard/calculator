9da75e7bb60d3e6b97c22c8c18a7495d45f9293b
THyldgaard <t.hyldgaard92@gmail.com>
2016-02-26 22:00:00 +0100

    written the @IBOutlets for all buttons and the sum label

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -20,6 +20,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var sumLabel: UILabel!
+    
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var plusMinusButton: UIButton!
+    @IBOutlet weak var procentButton: UIButton!
+    @IBOutlet weak var multiplyButton: UIButton!
+    
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var commaButton: UIButton!
+    @IBOutlet weak var equalButton: UIButton!
+    
+    // update label
+    // delete previous input
+    // addition
+    // subtraction
+    // division
+    // multiplication
+    // times hundred percent
+    // AC clear everything
+    // C clear previous number, when C x 3 == AC
+    // positiv or negativ number sign
+    
 
 }
 
